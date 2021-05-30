3c95d4a59e2e61880543df65540a3447a3ade11e
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 12:19:09 +0530

    python comments

---
+++ 02_comment.py
@@ -0,0 +1,15 @@
+# Single line commnet with '#'
+# Author : Nitin
+# Licenced to  : ABC Company
+
+
+# Multiple line comment using with ''' comment '''
+'''Author : Nitin
+Licenced to  : ABC Company
+*************Thanks for reading**********'''
+
+
+'''Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky'''
