0ccf86e4b07551088e3fe0c7626123be76dac46d
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 16:51:25 +0530

    tuple methods in python

---
+++ 19_tuple_methods.py
@@ -0,0 +1,4 @@
+# Creating a tuple using ()
+a=(1,2,4,5,6,8,1,5,1)
+print(a.count(5))#count method-->It will return number of times 5 occurs in a.
+print(a.index(1))#index method-->It will return the index of first occurrence of 1 in a.
