4f9f164000ad845ff6ec73f0d4e7ad195ccf21b2
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-31 02:45:16 +0530

    string strip function example added

--- 12_string_functions.py
+++ 12_string_functions.py
@@ -7,3 +7,9 @@ print(story.count("upon"))
 print(story.capitalize())  #first letter capital of string
 print(story.find("upon"))  #find upon and return upon index position --> doesn't exist it will return  -1
 print(story.replace("Nitin", "NMewada")) 
+
+
+
+story1='           once upon upon a time there was a Coder named Nitin who created python code        '
+print(story1)
+print(story1.strip()) #remove white space from start and end
