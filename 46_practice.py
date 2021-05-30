b5dab83126b3d0c5b4711b04e084533d4678e3e5
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-31 01:49:03 +0530

    reverse order multiplication table added

--- 46_practice.py
+++ 46_practice.py
@@ -4,3 +4,13 @@ num = int(input("Enter the number"))
 for i in range(1, 11):
     # print(str(num) + " X " + str(i) + "=" + str(i*num))
     print(f"{num}X{i}={num*i}") #f string
+
+
+
+
+# Write a program to print the multiplication table of a given number using for loop reverse order.
+
+num = int(input("Enter the number"))
+for i in range(10, 0,-1):
+    # print(str(num) + " X " + str(i) + "=" + str(i*num))
+    print(f"{num}X{i}={num*i}") #f string
