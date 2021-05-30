9f385f3687ff582c6bf1493e6c8a021cc523e815
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 16:49:00 +0530

    lists in python

---
+++ 15_lists.py
@@ -0,0 +1,18 @@
+# List
+# Create a list using []
+a = [1, 2 , 4, 50, 6]
+print(a) # Print the list using print() function
+
+# List Indexing
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+
+# Change the value of list using
+a[0] = 90
+print(a)
+
+
+# We can create a list with items of different types
+b = [15, "Nitin", False, 6.9]
+print(b)
