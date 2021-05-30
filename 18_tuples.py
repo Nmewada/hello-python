5983e1ada145871828e214437d410f9ca992808f
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 16:50:59 +0530

    tuples in python

---
+++ 18_tuples.py
@@ -0,0 +1,19 @@
+# Creating a tuple using ()
+t = (1, 2, 4, 5)
+print(t)
+
+t1 = () # Empty tuple
+
+# t2 = (1) # Wrong way to declare a Tuple with Single element
+
+t2 = (1,) # Tuple with Single element
+print(t2)
+
+
+# Printing the elements of a tuple
+print(t[3])
+
+
+
+# Cannot update the values of a tuple
+t[0] = 30 # throws an error
