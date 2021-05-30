8bd899586385592012e83e89d2487e339eddb83a
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 12:21:55 +0530

    python variables

---
+++ 05_variable.py
@@ -0,0 +1,31 @@
+a="nitin"                           #string single quote
+a1='nitin'                          #string double quote
+a2='''Hi 
+nitin
+Mewada'''                      #string triple quote
+b=22                                #integer
+c=40.10                             #float
+d=False                             #booleans
+e=None                              #None
+
+#printing the variables
+print(a)
+print(a1)
+print(a2)
+print(b)
+print(c)
+print(d)
+print(e)
+
+#Variable casesensetive
+A='Sandip'
+print(a)
+print(A)
+
+
+#printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
