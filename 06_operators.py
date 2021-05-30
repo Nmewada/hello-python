b0fceaa07e7af18c160aeb02555e5ba3dbec4a6a
nitin.mewada1992 <nitin.mewada1992@gmail.com>
2021-05-30 12:22:56 +0530

    operators in python

---
+++ 06_operators.py
@@ -0,0 +1,35 @@
+a=80
+b=40
+#Arithmetic  operators
+print("the value of a + b is",a+b)
+print("the value of a - b is",a-b)
+print("the value of a * b is",a*b)
+print("the value of a / b is",a/b)
+print("the remainder when a is divided by b is", a%b)
+
+#Assignment operators
+a=30
+a+=20
+print(a)
+a-=30
+print(a)
+a*=30
+print(a)
+a/=30
+print(a)
+
+#comparison operators
+# b = (14<=7)
+# b = (14>=7)
+# b = (14<7)
+# b = (14>7)
+# b = (14==7)
+b=(14<=7)
+print(b)
+
+#logical operator
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of not bool2 is", (not bool2))
